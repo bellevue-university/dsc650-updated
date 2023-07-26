d07f40aa7c9ebe6ffc0bd73256e57966158c6573
Nasheb Ismaily <nasheb@gmail.com>
2023-07-26 13:58:53 -0500

    Remove the directory

--- week1/resources/docker/bellvue-bigdata/hadoop-hive-spark-hbase/check_child_image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker image inspect --format='{{.RepoTags}} {{.Id}} {{.Parent}}' $(docker image ls -q --filter since=$1)
--- week1/resources/docker/bellvue-bigdata/hadoop-hive-spark-hbase/docker-compose-up-logging.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo 'param should be container name,like master,worker1 or worker2,or simply no param input at all'
-cname=$1
-docker-compose up -d && docker-compose logs -f $cname
