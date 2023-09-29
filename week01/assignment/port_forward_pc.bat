6542cba49058d88b4e7508c3bb330c81dfa0ee2d
Nasheb Ismaily <nasheb@gmail.com>
2023-09-28 20:52:04 -0500

    tested

--- week01/assignment/port_forward_pc.bat
+++ week01/assignment/port_forward_pc.bat
@@ -1,40 +1,48 @@
 @echo off
-setlocal enabledelayedexpansion
-
-REM Windows Script for Port Forwarding
+setlocal
 
 echo Enter your username:
 set /p username=
 echo Enter the external IP address:
 set /p external_IP=
-echo Enter the location of your private key (default: ~/.ssh/id_rsa):
+echo Enter the location of your private key (default: %USERPROFILE%\.ssh\id_rsa):
 set /p key_location=
 
-REM Default the private key location if not provided
-if "%key_location%"=="" set key_location=~/.ssh/id_rsa
+if "%key_location%"=="" set key_location=%USERPROFILE%\.ssh\id_rsa
 
-REM Define a subroutine to forward port and open browser
-:port_forward
-start ssh -i "%key_location%" -L %1:localhost:%1 %username%@%external_IP%
-timeout 3 >nul
-start http://localhost:%1%2
-goto:eof
+echo Forwarding for HDFS on port 9870
+start cmd.exe /k "ssh -i "%key_location%" -L 9870:localhost:9870 %username%@%external_IP% && echo Success || echo Error occurred. Press any key to close... && pause"
+timeout /t 3 >nul
+start http://localhost:9870
 
-REM HDFS
-call :port_forward 9870
-REM YARN
-call :port_forward 8088
-REM Spark Master
-call :port_forward 8080
-REM Spark History
-call :port_forward 18080
-REM HBASE
-call :port_forward 16010
-REM Solr
-call :port_forward 8983
-REM NIFI
-call :port_forward 8443 /nifi
+echo Forwarding for YARN on port 8088
+start cmd.exe /k "ssh -i "%key_location%" -L 8088:localhost:8088 %username%@%external_IP% && echo Success || echo Error occurred. Press any key to close... && pause"
+timeout /t 3 >nul
+start http://localhost:8088
 
-echo Port forwarding completed.
+echo Forwarding for Spark Master on port 8080
+start cmd.exe /k "ssh -i "%key_location%" -L 8080:localhost:8080 %username%@%external_IP% && echo Success || echo Error occurred. Press any key to close... && pause"
+timeout /t 3 >nul
+start http://localhost:8080
+
+echo Forwarding for Spark History on port 18080
+start cmd.exe /k "ssh -i "%key_location%" -L 18080:localhost:18080 %username%@%external_IP% && echo Success || echo Error occurred. Press any key to close... && pause"
+timeout /t 3 >nul
+start http://localhost:18080
+
+echo Forwarding for HBASE on port 16010
+start cmd.exe /k "ssh -i "%key_location%" -L 16010:localhost:16010 %username%@%external_IP% && echo Success || echo Error occurred. Press any key to close... && pause"
+timeout /t 3 >nul
+start http://localhost:16010
+
+echo Forwarding for Solr on port 8983
+start cmd.exe /k "ssh -i "%key_location%" -L 8983:localhost:8983 %username%@%external_IP% && echo Success || echo Error occurred. Press any key to close... && pause"
+timeout /t 3 >nul
+start http://localhost:8983
+
+echo Forwarding for NIFI on port 8443
+start cmd.exe /k "ssh -i "%key_location%" -L 8443:localhost:8443 %username%@%external_IP% && echo Success || echo Error occurred. Press any key to close... && pause"
+timeout /t 3 >nul
+start https://localhost:8443/nifi
+
 pause
-
